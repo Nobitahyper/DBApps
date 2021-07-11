d2200949fce2b3a0514f899df21dda0fbf16c0af
Nobitahyper <74531405+Nobitahyper@users.noreply.github.com>
2021-07-11 12:50:13 +0700

    Update course.php

--- course.php
+++ course.php
@@ -33,18 +33,7 @@
 
 
 <?php include  "navbar.php";
-
-$servername = "localhost";
-$username = "root";
-$password = "aman";
-$dbname = "student";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error){
-    die("conncetion failed: " . $conn->connnect_error);
-}
-echo "Connected Successfully <hr> <br>";
-?>
+ include "connection.php"; ?>
 
 <div><h2>Available Courses: </h2></div>
 
