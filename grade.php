eea918c63409ff0602858e09cb0c06bfea0ec115
Nobitahyper <74531405+Nobitahyper@users.noreply.github.com>
2021-07-11 12:50:46 +0700

    Update grade.php

--- grade.php
+++ grade.php
@@ -8,16 +8,7 @@
 <body>
 
 <?php include  "navbar.php";
- $servername = "localhost";
- $username = "root";
- $password = "aman";
- $dbname = "student";
- 
- $conn = new mysqli($servername, $username, $password, $dbname);
- if ($conn->connect_error){
-     die("conncetion failed: " . $conn->connnect_error);
- }
- echo "Connected Successfully <hr> <br>";
+include "connection.php";
  ?>
 
 <div><h2><br>Check your grade</h2></div>
