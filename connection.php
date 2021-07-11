0dc4311f8bffafb0c538d51d2893042ea37f2e9b
Nobitahyper <74531405+Nobitahyper@users.noreply.github.com>
2021-07-11 12:52:19 +0700

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "aman";
+$dbname = "student";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error){
+    die("conncetion failed: " . $conn->connnect_error);
+}
+echo "Connected Successfully <hr> <br>";
+?>
